a58044326a424692b60ec4c2f3e95a0328171b47
Alex Bonney <bonney977@gmail.com>
2021-10-08 18:06:16 +0300

    sorting enebled

--- Shared/Screens/Words/DictionaryViewModel.swift
+++ Shared/Screens/Words/DictionaryViewModel.swift
@@ -9,55 +9,18 @@ import SwiftUI
 import Combine
 
 class DictionaryManager: ObservableObject {
-        
+    //    @Environment(\.managedObjectContext) private var viewContext
+    //
+    //    @FetchRequest(
+    //        sortDescriptors: [NSSortDescriptor(keyPath: \Word.timestamp, ascending: true)],
+    //        animation: .default)
+    //    private var words: FetchedResults<Word>
     @Published var status: FetchingStatus = .blank
     @Published var inputWord: String = ""
     @Published var resultWordDetails: WordElement?
-//    @Published var definitions: [String] = []
+    @Published var sortingState: SortingCases = .def
     
     var cancellables = Set<AnyCancellable>()
-    
-//    func fetchData() {
-//        status = .loading
-//        if inputWord != "" {
-//            let stringURL = "https://api.dictionaryapi.dev/api/v2/entries/en/\(inputWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines))"
-//            guard let url = URL(string: stringURL) else {
-//                DispatchQueue.main.async {
-//                    self.status = .error
-//                    print("1 URL: \(stringURL)")
-//                }
-//                return
-//            }
-//            URLSession.shared.dataTask(with: url) { data, response, error in
-//                guard let data = data else {
-//                    DispatchQueue.main.async {
-//                        self.status = .error
-//                        print("2 URL: \(stringURL)")
-//                        print(error?.localizedDescription)
-//                    }
-//                    return
-//                }
-//
-//                do {
-//                    let decodedData = try JSONDecoder().decode(Words.self, from: data)
-//                    DispatchQueue.main.async {
-//                        self.resultWordDetails = decodedData.first!
-//                        self.status = .ready
-//                    }
-//
-//                } catch let error {
-//                    DispatchQueue.main.async {
-//                        self.resultWordDetails = nil
-//                        self.status = .error
-//                        print("3 URL: \(stringURL)")
-//                    }
-//                    print(error.localizedDescription)
-//                }
-//            }.resume()
-//        } else {
-//            return
-//        }
-//    }
     
     func fetchData() throws {
         status = .loading
@@ -91,165 +54,6 @@ class DictionaryManager: ObservableObject {
             }
             .store(in: &cancellables)
     }
-    
-//    func sort(by what: SortingCases) {
-//        DispatchQueue.main.async {
-//            withAnimation() {
-//                switch what {
-//                case .def:
-//                    self.sortingState = .def
-//                    self.words.sort {
-//                        $0.date < $1.date
-//                    }
-//                    self.filteredWords.sort {
-//                        $0.date < $1.date
-//                    }
-//                case .name:
-//                    self.sortingState = .name
-//                    self.words.sort {
-//                        $0.word < $1.word
-//                    }
-//                    self.filteredWords.sort {
-//                        $0.word < $1.word
-//                    }
-//                case .partOfSpeech:
-//                    self.sortingState = .partOfSpeech
-//                    self.words.sort {
-//                        $0.partOfSpeech < $1.partOfSpeech
-//                    }
-//                    self.filteredWords.sort {
-//                        $0.partOfSpeech < $1.partOfSpeech
-//                    }
-//                }
-//            }
-//        }
-//    }
-    
-//    func filter(by filter: FilterCases) {
-//        DispatchQueue.main.async {
-//            withAnimation() {
-//                var startArray = self.words
-//
-//                switch filter {
-//                case .none:
-//                    self.filterState = .none
-//                    startArray = self.words
-//                case .noun:
-//                    self.filterState = .noun
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "noun"})
-//                case .verb:
-//                    self.filterState = .verb
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "verb"})
-//                case .adjective:
-//                    self.filterState = .adjective
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "adjective"})
-//                case .adverb:
-//                    self.filterState = .adverb
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "adverb"})
-//                case .exclamation:
-//                    self.filterState = .exclamation
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "exclamation"})
-//                case .conjunction:
-//                    self.filterState = .conjunction
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "conjunction"})
-//                case .pronoun:
-//                    self.filterState = .pronoun
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.partOfSpeech != "pronoun"})
-//                case .favorite:
-//                    self.filterState = .favorite
-//                    startArray = self.words
-//                    startArray.removeAll(where: {$0.isFavorite != true })
-//                }
-//
-//                self.filteredWords = startArray
-//            }
-//        }
-//    }
-    
-//    func getWords() {
-//        let fileName = getDocumentsDirectory().appendingPathComponent("words")
-//        let query = CKQuery(recordType: "DictionaryWords", predicate: NSPredicate(value: true))
-//
-//        do {
-//            let words = try Data(contentsOf: fileName)
-//            self.words = try JSONDecoder().decode([WordModel].self, from: words)
-//        }
-//        catch {
-//            // something went wrong and it can't load data from the documents directory
-//            print("\(error.localizedDescription)")
-//
-//            // try to load them from iCloud
-//        }
-//    }
-    
-//    func save() {
-//        do {
-//            let fileName = getDocumentsDirectory().appendingPathComponent("words")
-//            let words = try JSONEncoder().encode(self.words)
-//            try words.write(to: fileName, options: [.atomicWrite, .completeFileProtection])
-//
-//            let query = CKQuery(recordType: "DictionaryWords", predicate: NSPredicate(value: true))
-//            let record = CKRecord(recordType: "DictionaryWords")
-//            record.setValue(words, forKey: "words")
-//
-//
-//            dataBase.perform(query, inZoneWith: nil) { records, error in
-//                guard let records = records, error == nil else {
-//                    return
-//                }
-//
-//                guard !records.isEmpty else {
-//                    //if there are no records in database, save brand new
-//                    self.saveRecord(record)
-//                    return
-//                }
-//
-//                if records.count != 1 {
-//                    //there are some records. Remove all of them
-//                    records.forEach { record in
-//                        self.dataBase.delete(withRecordID: record.recordID) { recordID, error in
-//                            if recordID != nil, error == nil {
-//                                print("deleted all records")
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    // there is one
-//                    self.dataBase.delete(withRecordID: records.first!.recordID) { recordID, error in
-//                        if recordID != nil, error == nil {
-//                            print("deleted one record")
-//                        }
-//                    }
-//                }
-//                self.saveRecord(record)
-//            }
-//        }
-//        catch {
-//            print("\(error.localizedDescription)")
-//        }
-//    }
-    
-//    private func saveRecord(_ record: CKRecord) {
-//        dataBase.save(record, completionHandler: { record, error in
-//            if record != nil, error == nil {
-//                print("saved")
-//            } else {
-//                guard let error = error else { return }
-//                print(error.localizedDescription)
-//            }
-//        })
-//    }
-//
-//    private func updateDataWithNewOneFromCloud() {
-//        //check if data on device
-//    }
 }
 
 enum FetchingStatus {
@@ -259,20 +63,9 @@ enum FetchingStatus {
     case error
 }
 
-//enum SortingCases {
-//    case def
-//    case name
-//    case partOfSpeech
-//}
-//
-//enum FilterCases {
-//    case none
-//    case noun
-//    case verb
-//    case adjective
-//    case adverb
-//    case exclamation
-//    case conjunction
-//    case pronoun
-//    case favorite
-//}
+enum SortingCases {
+    case def
+    case name
+    case partOfSpeech
+}
+
--- Shared/Screens/Words/WordsListView.swift
+++ Shared/Screens/Words/WordsListView.swift
@@ -10,27 +10,31 @@ import CoreData
 
 struct WordsListView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Word.timestamp, ascending: true)],
         animation: .default)
     private var words: FetchedResults<Word>
     
-    private var searchWords = [Word]()
-    
     @StateObject var vm = DictionaryManager()
     @State private var showingAddSheet = false
     @State private var searchTerm = ""
-
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(searchTerm.isEmpty ? Array(words) : words.filter({
                     guard let wordItself = $0.wordItself else { return false }
-                    return wordItself.starts(with: searchTerm)
-                })) { word in
+                    return wordItself.starts(with: searchTerm)})
+                ) { word in
                     NavigationLink(destination: WordDetailView(word: word)) {
-                        Text(word.wordItself ?? "word")
+                        HStack {
+                            Text(word.wordItself ?? "word")
+                                .bold()
+                            Spacer()
+                            Text(word.partOfSpeech ?? "")
+                                .foregroundColor(.secondary)
+                        }
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -45,6 +49,54 @@ struct WordsListView: View {
                         Label("Add Item", systemImage: "plus")
                     }
                 }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Menu {
+                        Section {
+                            Label {
+                                Text("Sort By")
+                            } icon: {
+                                Image(systemName: "arrow.up.arrow.down")
+                            }
+                        }
+                        Section {
+                            Button {
+                                withAnimation {
+                                    vm.sortingState = .def
+                                    words.sortDescriptors = [SortDescriptor(\Word.timestamp)]
+                                }
+                            } label: {
+                                if vm.sortingState == .def {
+                                    Image(systemName: "checkmark")
+                                }
+                                Text("Default")
+                            }
+                            Button {
+                                withAnimation {
+                                    vm.sortingState = .name
+                                    words.sortDescriptors = [SortDescriptor(\Word.wordItself)]
+                                }
+                            } label: {
+                                if vm.sortingState == .name {
+                                    Image(systemName: "checkmark")
+                                }
+                                Text("Name")
+                            }
+                            Button {
+                                withAnimation {
+                                    vm.sortingState = .partOfSpeech
+                                    words.sortDescriptors = [SortDescriptor(\Word.partOfSpeech)]
+                                }
+                            } label: {
+                                if vm.sortingState == .partOfSpeech {
+                                    Image(systemName: "checkmark")
+                                }
+                                Text("Part of speech")
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
+                    }
+                }
             }
             .sheet(isPresented: $showingAddSheet) {
                 AddView(vm: vm)
@@ -53,15 +105,14 @@ struct WordsListView: View {
         }
         .searchable(text: $searchTerm)
     }
-
+    
     private func addItem() {
         showingAddSheet = true
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { words[$0] }.forEach(viewContext.delete)
-
             do {
                 try viewContext.save()
             } catch {
